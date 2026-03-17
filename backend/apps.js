40d64103c506d02f2cda6f8a49fe0f9ecb686697
gvze7g <20230315@ricaldone.edu.sv>
2026-03-17 11:01:34 -0600

    CRUD de pizzas completado

--- backend/apps.js
+++ backend/apps.js
@@ -1,9 +1,13 @@
 import express from "express";
+import pizzaRoutes from "./src/routes/pizzas.js"
 
 //Creo una constante que es igual a
 //La libreria Express
 const app = express();
 
-app.use("/api/pizzas") ;
+//Para que la API acepte json
+app.use(express.json())
+
+app.use("/api/pizzas", pizzaRoutes) ;
 
 export default app;
--- backend/src/routes/pizzas.js
+++ backend/src/routes/pizzas.js
@@ -1,15 +1,16 @@
 import express from "express";
+import pizzasController from "../controllers/pizzasController.js";
 
 //Router() nos ayuda a colocar los metodos que tendrá el endpoint
 
 const router = express.Router();
 
 router.route("/")
-.get()
-.post()
+.get(pizzasController.getPizzas)
+.post(pizzasController.insertPizza)
 
 router.route("/:id")
-.put()
-.delete()
+.put(pizzasController.updatePizza)
+.delete(pizzasController.deletePizza)
 
 export default router;
