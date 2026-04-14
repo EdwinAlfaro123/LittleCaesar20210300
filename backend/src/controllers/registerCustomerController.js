ef30276b623982d3abe5bd3df77e1b99238fa09a
= <=>
2026-04-14 08:24:01 -0600

    creacion de cookie

--- backend/src/controllers/registerCustomerController.js
+++ backend/src/controllers/registerCustomerController.js
@@ -38,8 +38,83 @@ registerCustomerController.register = async (req, res) => {
         )
 
         //Guardamos el token en una cookie
-        res.cookie("RegistrationCookie", token, {maxAge: 15 * 60 * 1000})
-    } catch (error) {
+        res.cookie("registrationCookie", token, {maxAge: 15 * 60 * 1000})
         
+        //ENVIAR AL CORREO ELECTRONICO
+        //transporte -  Quien lo envia?
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: config.email.user_email, 
+                pass: config.email.user_password
+            }
+        })
+
+        //Quien lo recibe?
+        const mailOptions = {
+            from: config.email.user_email,
+            to: email,
+            subject: "Verificacion de cuenta",
+            text: "Para verificar tu cuenta, utiliza este codigo " + 
+            randomCode + "Expira en 15 minutos"
+        }
+
+        //Enviar el correo electronico
+        transporter.sendMail(mailOptions, (error, info) => {
+            if(error){
+                console.log("error" + error)
+                return res.status(500).json({message: "Error sending email"})
+            }
+
+            return res.status(200).json({message: "Email sent"})
+        })
+    } catch (error) {
+        console.log("error" + error)
+        return res.status(500).json({message: "Internal server error"})
     }
 }
+
+//Verificar el codigo que acabamos de enviar
+registerCustomerController.verifyCode = async (req, res) => {
+    try {
+        //Obtenermos el codigo que el usuario escribio en el frotend
+        const {verificationCodeRequest} = req.body
+
+        //Obtener el token de las cookies
+        const token = req.cookie.registrationCookie
+
+        //Extraer todos los datos del token
+        const decoded = jsonwebtoken.verify(token, config.JWT.secret)
+        const{
+            randomCode: storedCode,
+            name,
+            lastName,
+            birthdate,
+            email,
+            password,
+            isVerified,
+            loginAttemps,
+            timeOut
+        } = decoded
+
+        if(verificationCodeRequest !== storedCode){
+            return res.status(400).json({message: "Invalid Code"})
+        }
+
+        //Si todo esta bien, y el usuario, lo registramo en la DB
+        const newCustomer = customerModel({
+            name, lastName, birthdate, email, password, isVerified: true
+        })
+
+        await newCustomer.save()
+
+        res.clearCokkie("registrationCookie")
+
+        return res.status(200).json({message: "Customer registred"})
+    } catch (error) {
+        console.log("error" + error)
+        return res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export default registerCustomerController
